bbaf2d1284be776f65b4476e88c0cabcaa8eeba7
nalubowa proscovia <nalubowaprossyz@gmail.com>
2022-09-08 13:06:01 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+# include <stdio.h>
+/**
+ * main - printing
+ *
+ * Return: ! Always 0 (Successful)
+ */
+int main(avoid)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
