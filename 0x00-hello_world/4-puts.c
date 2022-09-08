ef851bc6be272d6e63836cfdf5e15096119a885c
nalubowa proscovia <nalubowaprossyz@gmail.com>
2022-09-08 14:35:18 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-# include <stdio.h>
-/**
- * main - Printing
- *
- * Return: 0 Aways 0 (Successful)
- */
-int main(void)
-{       
-	puts("\"Programming is like building a multilingual puzzle");
+#include <stdio.h> 
+/** 
+ * main - Printing 
+ * 
+ * Return: Always 0 (Successful) 
+ */ 
+int main(void) 
+{ 
+	puts("\"Programming is like building a multilingual puzzle"); 
 	return (0);
-}	
+}
+
